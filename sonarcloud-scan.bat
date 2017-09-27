4c07a1d0b04c1e6889ae04c3e8539835a4a80808
CosminSontu <sontu.cosmin@gmail.com>
2017-09-27 22:13:38 +0300

    Delete sonarcloud-scan.bat
    removed

--- sonarcloud-scan.bat
+++
@@ -1,5 +0,0 @@
-d:\_apps\sonarqube\bin\SonarQube.Scanner.MSBuild.exe begin /k:"CosminSontu:SpecDrill" /o:"cosminsontu-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="7912897f13c182e8daa0a6f958761bb021cfca68"
-
-MsBuild.exe /t:Rebuild
-
-d:\_apps\sonarqube\bin\SonarQube.Scanner.MSBuild.exe end /d:sonar.login="7912897f13c182e8daa0a6f958761bb021cfca68"
